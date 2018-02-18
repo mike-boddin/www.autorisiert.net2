aa58009ca49f2770c0826482f9f9b5548f208674
Mike Boddin <mike.boddin@devlab.expert>
2018-02-18 23:44:06 +0100

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var path = require('path');
+const path = require('path');
 
 module.exports = {
     entry: './src/js/index.js',
     plugins: [
-      new CopyWebpackPlugin([{from: "src/index.html", to: path.resolve(__dirname, 'dist')}])
-  ] ,
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+        new CopyWebpackPlugin([{from: "src/index.html", to: path.resolve(__dirname, 'dist')}])
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            }
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
