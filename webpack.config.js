f92f27950574a3cddcb2ff38665df63fa0df050f
Mike Boddin <mike.boddin@devlab.expert>
2018-02-19 23:07:16 +0100

    bootstrap, sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['./src/js/index.js', './src/scss/custom.scss'],
     plugins: [
-        new CopyWebpackPlugin([{from: "src/index.html", to: path.resolve(__dirname, 'dist')}])
+        new CopyWebpackPlugin([{ from: "src/index.html", to: path.resolve(__dirname, 'dist') }]),
+        new ExtractTextPlugin("styles.css"),
     ],
     module: {
         rules: [
@@ -18,6 +20,14 @@ module.exports = {
                         presets: ['@babel/preset-env'],
                     }
                 }
+            },
+            { // sass / scss loader for webpack
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
             }
         ],
     },
